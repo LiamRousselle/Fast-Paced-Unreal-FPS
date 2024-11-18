c34140c4b15e10a2ffbf253d13bbba1f9b28dab3
Liam <rousselle.mail@gmail.com>
2024-11-17 18:17:03 -0700

    implemented my old spring library

---
+++ Source/EngineSucks/Utilities/Spring.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Spring.h"
+
+void USpring::Update(float deltaTime)
+{
+	float scaledDeltaTime = FMath::Min(deltaTime, 1.f) * Speed / SPRING_ITERATIONS;
+	
+	for (int32 i = 0; i < SPRING_ITERATIONS; i++)
+	{
+		FVector iterationForce = Target - Position;
+		FVector acceleration = (iterationForce * Force) / Mass;
+		acceleration -= Velocity * Damping;
+
+		Velocity += acceleration * scaledDeltaTime;
+		Position += Velocity * scaledDeltaTime;
+	}
+}
---
+++ Source/EngineSucks/Utilities/Spring.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Spring.generated.h"
+
+#define SPRING_ITERATIONS					8.f
+
+UCLASS()
+class ENGINESUCKS_API USpring : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	USpring() = default;
+	
+	void SetProperties(
+		float mass = 5.f,
+		float force = 50.f,
+		float damping = 4.f,
+		float speed = 4.f
+	)
+	{
+		Mass = mass;
+		Force = force;
+		Damping = damping;
+		Speed = speed;
+	};
+	
+	void Update(float deltaTime);
+
+	void Shove(FVector direction) { Velocity += direction; };
+	
+	FVector Target = FVector();
+	FVector Position = FVector();
+	FVector Velocity = FVector();
+	
+protected:
+	float Mass = 5.f;
+	float Force = 50.f;
+	float Damping = 4.f;
+	float Speed = 4.f;
+};
